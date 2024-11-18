c6d4cf1fe20f5dec83a45909a14f895e10ad8a12
RicoAI16 <kd1384338@st.kobedenshi.ac.jp>
2024-11-18 14:59:45 +0900

    Add TestController and test page setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
+// Default route for the welcome page
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route for the TestController index method
+Route::get('/test', [TestController::class, 'index']);
