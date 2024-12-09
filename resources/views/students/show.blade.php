b82d20ab0facced4d18f7c62ab5c58bcb8a09983
RicoAI16 <kd1384338@st.kobedenshi.ac.jp>
2024-12-09 14:41:49 +0900

    Cleaned up Student Management files

--- resources/views/students/show.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<h1>{{ $student->name }}</h1>
-<p>年齢: {{ $student->age }}</p>
-<p>学科: {{ $student->major }}</p>
-<a href="/students">戻る</a>
-@endsection
